e0516a6bd5298d1c1ff1611eec815543deecd5db
waweroh <scadmsez@gmail.com>
2023-05-03 10:27:27 +0300

    feat: add best of feature

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-import random
+import random 
+import math
 
 def game():
     player = input("What's your choice? 'r' for rock, 'p' for paper, 's 'for scissors\n ")
@@ -7,19 +8,49 @@ def game():
     bot = random.choice (["r", "p", "s"])
 
     if player == bot:
-        return(f"Both of you have the same choice {player}. It's a tie!")
+        return(0, player, bot)
     
-    if player_wins(player, bot):
-        return(f"You have chosen {player} and the bot {bot}.You won.")
+    if player_won(player, bot):
+        return(1,player, bot)
     
-    return(f"You have chosen {player} and the bot {bot}.The bot wins.")
+    return(-1,player, bot)
 
-def player_wins(player, bot):
+def player_won(player, bot):
     #return true if player beats bot.
     #winning :r>s, s>p, p>r
     if (player == "r" and bot == "s") or (player == "p" and bot == "r") or (player == "s" and bot == "p"):
         return True
     return False
 
+def play_best_of(n):
+    # use ceil(n/2) which returns the smallest integer greater than or equal to a given number
+    player_wins = 0
+    bot_wins = 0
+    wins_needed = math.ceil(n/2)
+    while player_wins < wins_needed and bot_wins < wins_needed:
+        result, player, bot = game()
+        #tie
+        if result == 0:
+            print(f"It's a tie, you both chose {bot}")
+        #player wins
+        elif result == 1:
+            player_wins += 1 #increment wins.
+            print(f"You chose {player} and the bot chose {bot}. You win.")
+        #bot wins 
+        else:
+            #result == -1
+            bot_wins += 1 #increment wins
+            print(f"You chose {player} and the bot chose {bot}. Bot win.")
+    if player_wins >bot_wins:
+        print(f"You have won best of {n}. Way to go!")
+    else: 
+        print(f"The bot has won best of {n}. Izaa buda!")
+
+
+
+        
+
+
+
 if __name__ == "__main__":
-    print(game())
+    print(play_best_of(3))
